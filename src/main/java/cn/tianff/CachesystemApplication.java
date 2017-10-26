5c5ed4631528e66bb78618ad526c2c0f3d9bc1a1
tianff <307625876@qq.com>
2017-10-26 19:27:27 +0800

    Project Init

---
+++ src/main/java/cn/tianff/CachesystemApplication.java
@@ -0,0 +1,12 @@
+package cn.tianff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachesystemApplication.class, args);
+	}
+}
